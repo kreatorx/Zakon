ffca1b24ca4cd34ff95135c63a3abac5f9dc1655
kreatorx <husalmedin@gmail.com>
2026-06-27 21:21:32 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        model: 'gpt-5-mini',
+        model: 'gpt-5.4-nano',
         messages,
         response_format,
         temperature
