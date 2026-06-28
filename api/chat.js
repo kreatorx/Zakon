6ca6209f0cb80ffa92fb278891bc720c66d45a44
kreatorx <husalmedin@gmail.com>
2026-06-29 01:23:09 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-
+console.log("API CHAT HIT");
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
